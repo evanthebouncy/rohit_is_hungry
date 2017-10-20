2bf80e995f121d8577565a96205bde25d62919d6
Zachery Miranda <zmiranda@dhcp-18-189-118-66.dyn.MIT.EDU>
2017-10-19 22:22:57 -0400

    possible bug alleviation?

--- regex/generator.py
+++ regex/generator.py
@@ -11,7 +11,6 @@ def get_param():
 
 
 def check_example(params, example):
-  print 'am i really stuck here ?'
   '''Checks if an example is parameterized by these params
   Args:
     params: list of 6 parameters
@@ -19,17 +18,12 @@ def check_example(params, example):
   Return:
     True if a match is found and it completely captures the example
   '''
-  print 'stuck ehre?'
-  regex = '(^(({}{}{})*({}{}{})*)*$)'.format(*params)
-  print 'stuck ehre? 2'
-  print regex, example
+  regex = '((?:(?:{}{}{})*(?:{}{}{})*)*)'.format(*params)
+  # print regex, example
   matcher = re.match(regex, example)
-  print 'stuck ehre? 3'
   if matcher is None:
-    print 'stuck ehre? 4'
     return False
   else:
-    print 'stuck ehre? 5'
     # first matched group is the biggest
     return matcher.groups()[0] == example
 
@@ -61,7 +55,6 @@ def generate_positive_example(params):
 
 
 def generate_negative_example(params, p1=0.5, p2=0.1, p3=0.1, rec_depth=0):
-  print 'a'
   if rec_depth > 100:
     return None
   '''Creates an example that does not follow the regex
@@ -80,62 +73,58 @@ def generate_negative_example(params, p1=0.5, p2=0.1, p3=0.1, rec_depth=0):
     assert 0
 
   if random.random() < p1:
-    print 'b'
     example = list(generate_positive_example(params))
-    print 'bb'
     for i in xrange(len(example)):
       if random.random() < p2:
         example[i] = flip(example[i])
-    print 'bbb'
     example = reduce(lambda x,y:x+y, example, '')
-    print 'bbbb'
   else:
-    print 'c'
     new_params = [] + params
     for i in xrange(len(new_params)):
       if random.random() < p3:
         new_params[i] = get_param()
     example = generate_positive_example(new_params)
 
-  print 'bbbbb'
   if check_example(params, example):
-    print params, example, rec_depth
     return generate_negative_example(params, p1=p1, p2=p2, p3=p3, rec_depth=rec_depth+1)
   else:
-    print '6b'
     return example
 
 
 if __name__ == '__main__':
   # TEST!
   params = generate_params()
-  print params
-  print '((({}{}{})*({}{}{})*)*)'.format(*params)
-  pos = [generate_positive_example(params) for i in xrange(50)]
-  neg = [generate_negative_example(params) for i in xrange(50)]
+  # print params
+  # print '((({}{}{})*({}{}{})*)*)'.format(*params)
+  # pos = [generate_positive_example(params) for i in xrange(50)]
+  # neg = [generate_negative_example(params) for i in xrange(50)]
 
-  print "pos ", pos
-  print "neg ", neg
+  # print "pos ", pos
+  # print "neg ", neg
 
-  print 'Testing positive...'
-  correct_pos = True
-  for p in pos:
-    correct_pos &= check_example(params, p)
-  if not correct_pos:
-    print 'something is wrong with positive examples'
+  # print 'Testing positive...'
+  # correct_pos = True
+  # for p in pos:
+  #   correct_pos &= check_example(params, p)
+  # if not correct_pos:
+  #   print 'something is wrong with positive examples'
 
-  print 'Testing negative...'
-  correct_neg = True
-  for n in neg:
-    correct_neg &= not check_example(params, n)
-  if not correct_neg:
-    print 'something is wrong with negative examples'
+  # print 'Testing negative...'
+  # correct_neg = True
+  # for n in neg:
+  #   correct_neg &= not check_example(params, n)
+  # if not correct_neg:
+  #   print 'something is wrong with negative examples'
 
-  if correct_pos and correct_neg:
-    print 'Passed!'
-
+  # if correct_pos and correct_neg:
+  #   print 'Passed!'
+  import time
+  start = time.time()
   # can get stuck sometimes, for instance re.match('(^((00)*(10011)*)*$)', '000000000000000000000000000000000000000000000000000000000000000')
   for i in range(100000):
-    print i
     params = generate_params()
-    generate_negative_example(params)
+    e = generate_negative_example(params)
+    now = time.time()
+    if now-start > 0.001 and e is not None:
+        print i, now-start, params, e
+    start = now
