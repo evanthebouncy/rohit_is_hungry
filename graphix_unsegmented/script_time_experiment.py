012c44093e43a167ed66100ad72ae90492c9fed8
evanthebouncy <evanthebouncy@gmail.com>
2018-02-05 12:45:59 -0500

    we can re-run with this new thing which should be better

--- graphix_unsegmented/script_time_experiment.py
+++ graphix_unsegmented/script_time_experiment.py
@@ -5,48 +5,70 @@ import hand_examples
 from invert import *
 from graphix_lang import *
 import time
+import pickle
 
 assert not USING_SHOWCASE, "still using showcase params plz change meta_param.py "
 inverter = Inverter()
 
+TEST_LOC = "./data/data_test.p"
+def make_datas():
+  to_write = []
+  while len(to_write) < 400:
+    print len(to_write)
+    params = sample_params()
+    squares_orig,lines_orig = mk_scene(params)
+    rendered = render(squares_orig + lines_orig)
+    # only test on sufficiently complex, non-trivial images
+    if np.sum(rendered) < 100:
+      continue
+    else:
+      to_write.append(params)
+  pickle.dump(to_write, open(TEST_LOC, "wb"))
+
+# # make some datas
+# make_datas()
+# assert 0
+
 i = 0
-while True:
-  params = sample_params()
+test_data = pickle.load(open(TEST_LOC,"rb"))
+
+RESULT_LOC = "icml_result.p"
+to_dump = []
+
+for params in test_data:
   squares_orig,lines_orig = mk_scene(params)
   rendered = render(squares_orig + lines_orig)
-  # only test on sufficiently complex, non-trivial images
-  if np.sum(rendered) < 100:
-    continue
   constraints = img_2_constraints(rendered)
 
   i += 1
 
   draw_orig(rendered, "hand_drawings/target.png")
+  print "i with pixels ", np.sum(rendered)
 
-  i_full_params = inverter.invert_full(constraints, rendered, "full")
-  inverter.clear_solver()
-  i_rand_params = inverter.invert_full(constraints, rendered, "rand")
-  inverter.clear_solver()
-  i_nn_params = inverter.invert_full(constraints, rendered, "nn", 0.9)
-  inverter.clear_solver()
-  i_cegis_params = inverter.invert_cegis(constraints, rendered, "cegis")
-  inverter.clear_solver()
-  i_rcegis_params = inverter.invert_cegis(constraints, rendered, "r_cegis")
-  inverter.clear_solver()
-  i_acegis_params = inverter.invert_cegis(constraints, rendered, "a_cegis")
-  inverter.clear_solver()
-  i_rand_cegis_params = inverter.invert_full(constraints, rendered, "rand+cegis")
-  inverter.clear_solver()
-  i_nn_cegis_params = inverter.invert_full(constraints, rendered, "nn+cegis", 0.9)
-  inverter.clear_solver()
+  to_add = [
+      # inverter.invert_full(constraints, rendered, "full"),
+      # inverter.invert_full(constraints, rendered, "rand"),
+      # inverter.invert_full(constraints, rendered, "nn", 0.9),
+      # inverter.invert_cegis(constraints, rendered, "cegis"),
+      # inverter.invert_cegis(constraints, rendered, "r_cegis"),
+      # inverter.invert_cegis(constraints, rendered, "a_cegis"),
+      # inverter.invert_full(constraints, rendered, "rand+cegis"),
+  inverter.invert_full(constraints, rendered, "h1+cegis"),
+  inverter.invert_full(constraints, rendered, "nn+cegis", 0.9),
+      ]
 
+  if len(to_dump) == 0:
+    to_dump = [[] for _ in range(len(to_add))]
+  
+  assert len(to_dump) == len(to_add)
+  for jjj in range(len(to_dump)):
+    to_dump[jjj].append(to_add[jjj])
 
-  print "===================================== number {} with {} pixels ".format(i, np.sum(rendered)) 
-  print "time_full: ", i_full_params['building_time'], i_full_params['solving_time'], i_full_params['ce_size']
-  print "time_rand: ", i_rand_params['building_time'], i_rand_params['solving_time'], i_rand_params['ce_size'], i_rand_params['error'] / 1024
-  print "time_nn: ", i_nn_params['building_time'], i_nn_params['solving_time'], i_nn_params['ce_size'], i_nn_params['nn_time']
-  print "time_cegis: ", i_cegis_params['building_time'], i_cegis_params['solving_time'], i_cegis_params['ce_size']
-  print "time_rcegis: ", i_rcegis_params['building_time'], i_rcegis_params['solving_time'], i_rcegis_params['ce_size']
-  print "time_acegis: ", i_acegis_params['building_time'], i_acegis_params['solving_time'], i_acegis_params['ce_size']
-  print "time_rand_cegis: ", i_rand_cegis_params['building_time'], i_rand_cegis_params['solving_time'], i_rand_cegis_params['ce_size']
-  print "time_nn_cegis: ", i_nn_cegis_params['building_time'], i_nn_cegis_params['solving_time'], i_nn_cegis_params['ce_size'], i_nn_cegis_params['nn_time'], i_nn_cegis_params['error'] / 1024, i_nn_cegis_params['orig_subset_size']
+  for blahblah in to_dump:
+    print sorted(list(blahblah[-1].items()))
+
+  if i % 2 == 0:
+    print "dumping pickle result"
+    pickle.dump(to_dump, open(RESULT_LOC, "wb"))
+
+pickle.dump(to_dump, open(RESULT_LOC, "wb"))
