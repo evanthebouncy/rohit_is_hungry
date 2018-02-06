8370df4c4d0527fc64f4599b5db8c260b688171e
Zachery Miranda <exsmiley@gmail.com>
2018-02-05 22:38:28 -0500

    update to handle percent represented and make the network graphs

--- orderings/grapher.py
+++ orderings/grapher.py
@@ -9,6 +9,7 @@ def graph_stuff(TEST_LOC='results.p'):
     names = []
     num_examples = {}
     is_rep = {}
+    p_rep = {}
 
     for i, trial in enumerate(data):
         for name, stats in trial.iteritems():
@@ -16,29 +17,39 @@ def graph_stuff(TEST_LOC='results.p'):
                 names.append(name)
                 num_examples[name] = [stats['num_examples']]
                 is_rep[name] = [stats['is_rep']]
+                p_rep[name] = [stats['p_rep']]
             else:
                 num_examples[name].append(stats['num_examples'])
                 is_rep[name].append(stats['is_rep'])
+                p_rep[name].append(stats['p_rep'])
 
     ind = np.arange(len(names))
     num_examples = [num_examples[name] for name in names]
     is_rep = [1.0*sum(is_rep[name])/len(num_examples[0]) for name in names]
+    p_rep = [np.mean(p_rep[name]) for name in names]
 
     avg_sizes = [np.mean(nums) for nums in num_examples]
 
     width = 0.35
+    print avg_sizes
     plt.bar(ind, avg_sizes, width)
     plt.xticks(ind, names)
-    plt.title('Average Number of Examples Used')
+    plt.title('Average Percentage of Examples Used')
     plt.show()
 
     plt.boxplot(num_examples, labels=names)
-    plt.title('Distribution of Examples Used')
+    plt.title('Distribution of Percentage of Examples Used')
     plt.show()
 
     plt.bar(ind, is_rep, width)
     plt.xticks(ind, names)
-    plt.title('Percent Is Representative Set')
+    plt.title('Percent Is Completely Representative Set')
+    plt.show()
+
+    print p_rep, names
+    plt.bar(ind, p_rep, width)
+    plt.xticks(ind, names)
+    plt.title('Percent of Set Represented')
     plt.show()
 
 
