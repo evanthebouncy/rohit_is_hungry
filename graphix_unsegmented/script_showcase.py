5545ca63c16d5b895f994e8600a294e7f21a23f1
Zachery Miranda <exsmiley@gmail.com>
2018-02-07 17:07:55 -0500

    rcegis and nn then cegis for showcase

--- graphix_unsegmented/script_showcase.py
+++ graphix_unsegmented/script_showcase.py
@@ -19,60 +19,62 @@ inverter = Inverter()
 def run_comparison():
 
   stime = time.time()
-  inverted_params = inverter.invert_full(constraints, rendered, "nn+cegis")
+  inverted_params = inverter.invert_full(constraints, rendered, "nn+cegis", 0.9)
+  print inverted_params, 'ours'
   nn_cegis_time = time.time() - stime
   squares,lines = mk_scene(inverted_params)
   draw_orig(render(squares+lines), "hand_drawings/result_nn+cegis.png")
 
   inverter.clear_solver()
 
-  stime = time.time()
-  inverted_params = inverter.invert_full(constraints, rendered, "nn", 0.1)
-  nn_time = time.time() - stime
-  squares,lines = mk_scene(inverted_params)
-  draw_orig(render(squares+lines), "hand_drawings/result_full_nn.png")
+  # stime = time.time()
+  # inverted_params = inverter.invert_full(constraints, rendered, "nn", 0.1)
+  # nn_time = time.time() - stime
+  # squares,lines = mk_scene(inverted_params)
+  # draw_orig(render(squares+lines), "hand_drawings/result_full_nn.png")
 
-  inverter.clear_solver()
+  # inverter.clear_solver()
 
-  stime = time.time()
-  inverted_params = inverter.invert_full(constraints, rendered, "rand", 0.1)
-  rand_time = time.time() - stime
-  squares,lines = mk_scene(inverted_params)
-  draw_orig(render(squares+lines), "hand_drawings/result_full_rand.png")
+  # stime = time.time()
+  # inverted_params = inverter.invert_full(constraints, rendered, "rand", 0.1)
+  # rand_time = time.time() - stime
+  # squares,lines = mk_scene(inverted_params)
+  # draw_orig(render(squares+lines), "hand_drawings/result_full_rand.png")
 
-  inverter.clear_solver()
+  # inverter.clear_solver()
 
   stime = time.time()
   inverted_params = inverter.invert_cegis(constraints, rendered, "r_cegis")
+  print inverted_params, 'r_cegis'
   r_cegis_time = time.time() - stime
   squares,lines = mk_scene(inverted_params)
   draw_orig(render(squares+lines), "hand_drawings/result_r_cegis.png")
 
   inverter.clear_solver()
 
-  stime = time.time()
-  inverted_params = inverter.invert_cegis(constraints, rendered, "cegis")
-  cegis_time = time.time() - stime
-  squares,lines = mk_scene(inverted_params)
-  draw_orig(render(squares+lines), "hand_drawings/result_cegis.png")
+  # stime = time.time()
+  # inverted_params = inverter.invert_cegis(constraints, rendered, "cegis")
+  # cegis_time = time.time() - stime
+  # squares,lines = mk_scene(inverted_params)
+  # draw_orig(render(squares+lines), "hand_drawings/result_cegis.png")
 
-  inverter.clear_solver()
+  # inverter.clear_solver()
 
-  stime = time.time()
-  inverted_params = inverter.invert_full(constraints, rendered, "rand+cegis", 0.1)
-  rand_cegis_time = time.time() - stime
-  squares,lines = mk_scene(inverted_params)
-  draw_orig(render(squares+lines), "hand_drawings/result_rand+cegis.png")
+  # stime = time.time()
+  # inverted_params = inverter.invert_full(constraints, rendered, "rand+cegis", 0.1)
+  # rand_cegis_time = time.time() - stime
+  # squares,lines = mk_scene(inverted_params)
+  # draw_orig(render(squares+lines), "hand_drawings/result_rand+cegis.png")
 
-  inverter.clear_solver()
+  # inverter.clear_solver()
 
   print "all done times "
-  print "nn ", nn_time
-  print "rand ", rand_time
+  # print "nn ", nn_time
+  # print "rand ", rand_time
 
   print "rcegis ", r_cegis_time
-  print "cegis ",  cegis_time
+  # print "cegis ",  cegis_time
   print "nn then cegis ", nn_cegis_time
-  print "rand then cegis ", rand_cegis_time
+  # print "rand then cegis ", rand_cegis_time
 
 run_comparison()
